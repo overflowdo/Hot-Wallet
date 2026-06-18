3e05d3fd511933b1bc751f831eb95f3ba7b39678
overflowdo <overflodo@web.de>
2026-06-18 13:34:07 +0200

    dsc

--- scripts/testing/send_intent.sh
+++ scripts/testing/send_intent.sh
@@ -47,8 +47,8 @@ PAYLOAD=$(jq -n \
   --arg id "t1-amount deny" \
   --arg network "regtest" \
   --arg address "bcrt1qanotherexampledestinationaddress00000000000" \
-  --argjson amount 5000001 \
   --arg source "hot" \
+  --argjson amount 5000001 \
   '{
       id: $id,
       type: "hot-tx",
@@ -67,8 +67,8 @@ PAYLOAD=$(jq -n \
   --arg id "t1-target deny" \
   --arg network "regtest" \
   --arg address "bcrt1qanotherexampledestinationaddress00000000001" \
-  --argjson amount 1000 \
   --arg source "hot" \
+  --argjson amount 1000 \
   '{
       id: $id,
       type: "hot-tx",
@@ -87,8 +87,8 @@ PAYLOAD=$(jq -n \
   --arg id "t1-hot-tx allow" \
   --arg network "regtest" \
   --arg address "bcrt1qanotherexampledestinationaddress00000000000" \
-  --argjson amount 1000 \
   --arg source "hot" \
+  --argjson amount 1000 \
   '{
       id: $id,
       type: "hot-tx",
